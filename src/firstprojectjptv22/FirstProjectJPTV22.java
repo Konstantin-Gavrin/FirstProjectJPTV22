a182826e64acee3777a6f6f336e087cf87cbba18
pupil <pupil@ivkhk.ee>
2023-09-05 10:38:38 +0300

    Первый коммит моего репозитория

---
+++ src/firstprojectjptv22/FirstProjectJPTV22.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstprojectjptv22;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pupil
+ */
+public class FirstProjectJPTV22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Введите имя: ");
+        String firstname = scanner.nextLine();
+        System.out.print("Введите фамилию: ");
+        String lastname = scanner.nextLine();
+        System.out.print("Введите год рождения: ");
+        int birthdate = scanner.nextInt();
+        int currentYear = 2023;
+        int age = currentYear - birthdate;
+        
+        System.out.println("Hello, " + lastname + " " + firstname); 
+        System.out.println("Your age: " + age );
+    }
+    
+}
